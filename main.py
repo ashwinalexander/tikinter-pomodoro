b9f3f6906785696233d92968519e30e157ec4192
ashwin alexander <alexwin75@gmail.com>
2023-08-03 21:38:26 -0400

    initial UI setup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+# since we are working with many classes, import them all.
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +17,23 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+# since we have imported all classes, we do not need to tkinter.Tk() to access the Tk class
+window = Tk()
+window.title("pomodoro")
+# first configure the window
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# next configure the canvas
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+
+# next configure the image on the canvas
+filename = PhotoImage(file="tomato.png")
+canvas.create_image(100,112,image=filename)
+
+# next add some text over the image
+canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+canvas.pack()
+
+
+window.mainloop()
