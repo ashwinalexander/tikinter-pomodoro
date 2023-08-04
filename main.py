efd9365fa8f198fd6ef0cb5d2303c8b7e217c9be
ashwin alexander <alexwin75@gmail.com>
2023-08-03 21:54:44 -0400

    Adding some UI elements to make the app more usable'

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ window.title("pomodoro")
 # first configure the window
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# next add the Timer label
+lblTimer = Label(text="Timer", font=(FONT_NAME, 25, "bold"), fg=GREEN, bg=YELLOW)
+lblTimer.grid(row=0,column=1)
+
 # next configure the canvas
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 
@@ -32,8 +36,28 @@ canvas.create_image(100,112,image=filename)
 
 # next add some text over the image
 canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
 
-canvas.pack()
+def btnStart_Click():
+    print("button start clicked")
+
+def btnReset_Click():
+    print("button reset clicked")
+
+# next add the two buttons
+btnStart = Button(text="Start", command= btnStart_Click)
+btnStart.grid(row=2, column=0)
+
+btnReset = Button(text="Reset", command= btnReset_Click)
+btnReset.grid(row=2, column=2)
+
+# finally add the Check label
+lblTimer = Label(text="✔", font=(FONT_NAME, 12, "bold"), fg=GREEN, bg=YELLOW)
+lblTimer.grid(row=3,column=1)
+
+
+
+
 
 
 window.mainloop()
